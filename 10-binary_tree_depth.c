3a7cf43f6f699f08af48726bc9aba00418a4d4ee
Davey <david.c.hays.iii@gmail.com>
2024-04-13 19:07:18 +0000

    trailing whitespace

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth()- find the depth of a node
+ * @tree: the node in question
+ *
+ * Return: depth of node
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t i;
+	binary_tree_t tmp;
+
+	if (!tree || !tree->parent)
+	{
+		return (0);
+	}
+
+	i = 1;
+	tmp = tree;
+	while (tmp->parent != NULL)
+	{
+		i++;
+		tmp = tree->parent;
+	}
+	return (i);
+}
