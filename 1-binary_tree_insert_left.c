8f9a4129b60d24bcc7da435eda3e9f10c63a61f1
Davey <david.c.hays.iii@gmail.com>
2024-04-13 17:56:45 +0000

    added case for existing left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	parent->left = (binary_tree_node(parent, value));
-	if (parent->left == NULL)
+	binary_tree_t *new_node, *tmp;
+
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	return (parent->left);
+	new_node = binary_tree_node(parent, value);
+	tmp = parent->left;
+	if (tmp == NULL)
+	{
+		parent->left = new_node;
+		return (new_node);
+	}
+
+	parent->left = new_node;
+	new_node->left = tmp;
+	return (new_node);
 }
