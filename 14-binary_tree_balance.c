4b74dc1dd03218e1770a12471268077f07eae13d
Davey <david.c.hays.iii@gmail.com>
2024-04-16 19:38:36 +0000

    added file 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_balance()- gives the balance factor of a tree
+ * @tree: the tree in question
+ *
+ * Return: the balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l, r;
+
+	l = 0;
+	r = 0;
+
+	if (tree->left)
+		l = binary_tree_height(tree->left);
+	if (tree->right)
+		r = binary_tree_height(tree->right);
+
+	return ((l - r));
+
+}
