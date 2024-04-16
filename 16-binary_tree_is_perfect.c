7b81c1f2e3f663668ec553fd05787db9eec74d6f
Davey <david.c.hays.iii@gmail.com>
2024-04-16 22:33:24 +0000

    changing constant

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,7 +17,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		return (1);
-	if ((int)binary_tree_size(tree) ==  ((1 >> (int)binary_tree_height(tree)) - 1))
+	if ((int)binary_tree_size(tree) ==  ((1 >> (int)binary_tree_height(tree)) + 1))
 		return (1);
 
 	return (0);
