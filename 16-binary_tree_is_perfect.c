ec4a099530ee13a75748d5a113cbdee01927a209
Davey <david.c.hays.iii@gmail.com>
2024-04-16 22:29:59 +0000

    added case for single node

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,7 +15,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if ((int)binary_tree_size(tree) ==  (1 >> (int)binary_tree_height(tree)) - 1)
+	if (!tree->left && !tree->right)
+		return (1);
+	if ((int)binary_tree_size(tree) ==  ((1 >> (int)binary_tree_height(tree)) - 1))
 		return (1);
 
 	return (0);
