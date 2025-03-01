d2ea3e82ac07e49654dc783bc71f6529e9408849
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-28 22:17:16 -0600

    typo

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -20,5 +20,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	return (binary_tree_is_full(tree->left) &&
-			(binary_tree_is_full(tree->right);
+			binary_tree_is_full(tree->right));
 }
