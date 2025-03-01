30a8985dcd7d2df644749f71c791fa222856aaf0
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-28 22:24:26 -0600

    correction to 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,8 +17,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		return (1);
-	if ((int)binary_tree_size(tree) ==  ((1 >> (int)binary_tree_height(tree)) + 1))
-		return (1);
+	if (!binary_tree_is_full(tree))
+		return (0);
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+		return (0);
 
-	return (0);
+	return (1);
 }
