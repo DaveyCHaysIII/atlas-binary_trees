fa1689e7bbad60344f2c33e34b28968112694bdb
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-28 22:34:01 -0600

    made a small change to 15, seeing if it breaks

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (0);
+		return (1);
 
 	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 		return (0);
