4e8620973ce23422ae2bde41fbd755a4577e76c0
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-03-03 20:22:50 -0600

    changed height formula

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,6 +15,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (0);
+
 	left_branch = binary_tree_height(tree->left);
 	right_branch = binary_tree_height(tree->right);
 
